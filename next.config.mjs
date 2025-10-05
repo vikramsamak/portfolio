d87f787cd071dbcd9b0b59ec6fa165356f119e96
vikramsamak <vikramsamak02@gmail.com>
2025-10-05 15:15:32 +0530

    chore : add config for better seo.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizeCss: true,
+    inlineCss: true,
+  },
 };
 
 export default nextConfig;
